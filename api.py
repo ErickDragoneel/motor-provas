42fbe152fc46ed169cdda8eee5ad610263cb0454
ErickDragoneel <joaoerick064@gmail.com>
2026-03-11 13:58:40 -0300

    sistema simplificado

--- api.py
+++ api.py
@@ -1,3 +1,15 @@
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+import json
+import random
+import subprocess
+
+app = FastAPI()
+
+with open("banco_perguntas.json", "r", encoding="utf-8") as f:
+    perguntas = json.load(f)
+
+
 @app.get("/gerar_prova")
 def gerar_prova(materia: str, nivel: str, dificuldade: str, quantidade: int):
 
@@ -21,3 +33,15 @@ def gerar_prova(materia: str, nivel: str, dificuldade: str, quantidade: int):
         "dificuldade": dificuldade,
         "questoes": prova
     }
+
+
+@app.get("/baixar_pdf")
+def baixar_pdf(materia: str, nivel: str, dificuldade: str, quantidade: int):
+
+    subprocess.run(["python", "gerar_pdf.py", materia, nivel, dificuldade, str(quantidade)])
+
+    return FileResponse(
+        "prova.pdf",
+        media_type="application/pdf",
+        filename="prova.pdf"
+    )
--- gerar_prova.py
+++ gerar_prova.py
@@ -1,36 +1,71 @@
+import sys
 import json
 import random
+from reportlab.lib.pagesizes import letter
+from reportlab.pdfgen import canvas
+
+materia = sys.argv[1]
+nivel = sys.argv[2]
+dificuldade = sys.argv[3]
+quantidade = int(sys.argv[4])
 
 with open("banco_perguntas.json", "r", encoding="utf-8") as f:
     perguntas = json.load(f)
 
-materia = input("Escolha a matéria: ")
-
-quantidade = int(input("Quantas perguntas na prova? "))
-
 filtradas = []
 
 for p in perguntas:
-    if p["materia"] == materia:
+    if (
+        p["materia"].lower() == materia.lower()
+        and p["nivel"].lower() == nivel.lower()
+        and p["dificuldade"].lower() == dificuldade.lower()
+    ):
         filtradas.append(p)
 
-# evita erro se pedir mais perguntas do que existem
 quantidade = min(quantidade, len(filtradas))
 
 prova = random.sample(filtradas, quantidade)
 
-print("\nPROVA DO ALUNO\n")
+pdf = canvas.Canvas("prova.pdf", pagesize=letter)
 
-for i, p in enumerate(prova,1):
+y = 750
 
-    print(f"{i}. {p['pergunta']}")
+pdf.setFont("Helvetica-Bold", 16)
+pdf.drawString(100, y, f"Prova de {materia}")
 
-    for j, opcao in enumerate(p["opcoes"],1):
-        print(f"   {j}. {opcao}")
+y -= 30
 
-    print()
+pdf.setFont("Helvetica", 12)
+pdf.drawString(100, y, f"Nível: {nivel}")
 
-print("\nGABARITO\n")
+y -= 20
 
-for i, p in enumerate(prova,1):
-    print(f"{i}. {p['resposta']}")
+pdf.drawString(100, y, f"Dificuldade: {dificuldade}")
+
+y -= 40
+
+pdf.drawString(100, y, "Aluno: __________________________________")
+
+y -= 30
+
+pdf.drawString(100, y, "Data: ____ / ____ / ______")
+
+y -= 40
+
+for i, q in enumerate(prova, 1):
+
+    pdf.drawString(100, y, f"{i}. {q['pergunta']}")
+
+    y -= 20
+
+    for op in q["opcoes"]:
+        pdf.drawString(120, y, f"( ) {op}")
+        y -= 20
+
+    y -= 10
+
+    if y < 100:
+        pdf.showPage()
+        y = 750
+
+pdf.save()
