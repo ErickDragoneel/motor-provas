2ecc983572bdfff0f58ed22331ddeaadd887c488
ErickDragoneel <joaoerick064@gmail.com>
2026-03-11 13:42:37 -0300

    pdf atualizado

--- gerar_pdf.py
+++ gerar_pdf.py
@@ -1,45 +1,67 @@
+import sys
 import json
 import random
 from reportlab.lib.pagesizes import letter
 from reportlab.pdfgen import canvas
 
+materia = sys.argv[1]
+nivel = sys.argv[2]
+quantidade = int(sys.argv[3])
+
 with open("banco_perguntas.json", "r", encoding="utf-8") as f:
     perguntas = json.load(f)
-
-materia = input("Escolha a matéria: ")
-quantidade = int(input("Quantas perguntas na prova? "))
 
 filtradas = []
 
 for p in perguntas:
-    if p["materia"] == materia:
+    if (
+        p["materia"].lower() == materia.lower()
+        and p["nivel"].lower() == nivel.lower()
+    ):
         filtradas.append(p)
 
 quantidade = min(quantidade, len(filtradas))
 
 prova = random.sample(filtradas, quantidade)
 
-# criar PDF
 pdf = canvas.Canvas("prova.pdf", pagesize=letter)
 
 y = 750
 
-pdf.setFont("Helvetica", 12)
+pdf.setFont("Helvetica-Bold", 16)
 pdf.drawString(100, y, f"Prova de {materia}")
+
+y -= 30
+
+pdf.setFont("Helvetica", 12)
+pdf.drawString(100, y, f"Nível: {nivel}")
 
 y -= 40
 
-for i, p in enumerate(prova,1):
+pdf.drawString(100, y, "Aluno: ______________________________________")
 
-    pdf.drawString(100, y, f"{i}. {p['pergunta']}")
+y -= 30
+
+pdf.drawString(100, y, "Data: ____ / ____ / ______")
+
+y -= 40
+
+for i, q in enumerate(prova, 1):
+
+    pdf.drawString(100, y, f"{i}. {q['pergunta']}")
+
     y -= 20
 
-    for opcao in p["opcoes"]:
-        pdf.drawString(120, y, f"- {opcao}")
+    for op in q["opcoes"]:
+        pdf.drawString(120, y, f"( ) {op}")
         y -= 20
 
     y -= 10
 
+    if y < 100:
+        pdf.showPage()
+        y = 750
+
 pdf.save()
 
-print("PDF da prova criado com sucesso!")
+print("PDF gerado com sucesso")
